d2cb46a1c3e2a594a05554b27264b4d867685d4f
aximu4 <savvasav2010@gmail.com>
2025-08-15 00:17:39 +0300

    Update TesseraInstaller.lua

--- TesseraInstaller.lua
+++ TesseraInstaller.lua
@@ -3,12 +3,20 @@ local rawBase = "https://raw.githubusercontent.com/" .. repo .. "/main/"
 
 shell.run("wget", rawBase .. "Tessera.lua", "Tessera.lua")
 
-
-shell.run("mkdir", "Tessera")
-local files = {"music-a.nbs", "music-b.nbs", "music-c.nbs", "score.nbs"}
-for _, fname in ipairs(files) do
-  local url = rawBase .. "Tessera/" .. fname
-  shell.run("wget", url, "Tessera/" .. fname)
+if not fs.exists("Tessera") then
+  fs.makeDir("Tessera")
 end
+
+local files = {"music-a.nbs", "music-b.nbs", "music-c.nbs", "score.nbs"}
+
+local baseUrl = "https://raw.githubusercontent.com/aximu4/Tessera/main/Tessera/"
+
+for _, fname in ipairs(files) do
+  local success = shell.run("wget", baseUrl .. fname, "Tessera/" .. fname)
+  if not success then
+    print("Couldn't install " .. fname)
+  end
+end
+
 
 print("Tessera successfully installed! Run 'Tessera' to play.")
