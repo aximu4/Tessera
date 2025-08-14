425b7ef0b0ad6a10047023c601160efbd09edf70
aximu4 <savvasav2010@gmail.com>
2025-08-15 00:21:11 +0300

    Update TesseraInstaller.lua

--- TesseraInstaller.lua
+++ TesseraInstaller.lua
@@ -1,22 +1,25 @@
-local repo = "aximu4/Tessera"
-local rawBase = "https://raw.githubusercontent.com/" .. repo .. "/main/"
-
-shell.run("wget", rawBase .. "Tessera.lua", "Tessera.lua")
-
 if not fs.exists("Tessera") then
-  fs.makeDir("Tessera")
+    fs.makeDir("Tessera")
 end
 
-local files = {"music-a.nbs", "music-b.nbs", "music-c.nbs", "score.nbs"}
+local musicFiles = {"music-a.nbs", "music-b.nbs", "music-c.nbs", "score.nbs"}
 
 local baseUrl = "https://raw.githubusercontent.com/aximu4/Tessera/main/Tessera/"
 
-for _, fname in ipairs(files) do
-  local success = shell.run("wget", baseUrl .. fname, "Tessera/" .. fname)
-  if not success then
-    print("Couldn't install " .. fname)
-  end
+print("Downloading Tessera.lua...")
+local successMain = shell.run("wget", "https://raw.githubusercontent.com/aximu4/Tessera/main/Tessera.lua", "Tessera.lua")
+if not successMain then
+    print("Couldn't install Tessera.lua! Please check your internet connection.")
+    return
 end
 
+for _, file in ipairs(musicFiles) do
+    print("Скачиваем " .. file .. "...")
+    local success = shell.run("wget", baseUrl .. file, "Tessera/" .. file)
+    if not success then
+        print("Couldn't download " .. file .. ". Please try again later.")
+    end
+end
 
-print("Tessera successfully installed! Run 'Tessera' to play.")
+print("Tessera successfully installed!")
+print("Run 'Tessera' to play.")
